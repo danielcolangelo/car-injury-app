0d0b2c19b20c04c6cca63961e12c00f97ca1e31b
Daniel Colangelo <Danalfons@yahoo.com>
2017-03-18 09:05:37 -0400

    layout for the law app

---
+++ car-accident/car-accident/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  car-accident
+//
+//  Created by Danny Colangelo on 3/18/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func callBtn(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
